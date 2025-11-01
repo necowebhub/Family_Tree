f15eba4b7cc65cbaa3f80fbd4a81053fc165fb33
Snorkich <kakashka-x3@yandex.ru>
2025-11-01 10:49:13 +0200

    Added 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import PublicPage from "./pages/PublicPage";
 import AdminPage from "./pages/AdminPage";
+import NotFound from "./pages/404";
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<PublicPage />} />
             <Route path="/admin" element={<AdminPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>404 - Страница не найдена</h1>
+            <Link to="/">Вернуться на главную</Link>
+        </div>
+    );
+}
