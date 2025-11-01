6d6dfd7d61bd1fd6b1d134322fe705ecb3300f25
Snorkich <kakashka-x3@yandex.ru>
2025-11-01 11:26:32 +0200

    Updated 404 page added gitignore rules

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
-        <div>
+        <div style={{textAlign: "center", padding: "50px"}}>
             <h1>404 - Страница не найдена</h1>
             <Link to="/">Вернуться на главную</Link>
         </div>
