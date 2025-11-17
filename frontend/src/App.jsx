72920a384cca0601e3a981b445c68c170e717d92
Snorkich <kakashka-x3@yandex.ru>
2025-11-17 20:36:28 +0200

    Updated App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PublicPage from "./pages/PublicPage";
 import AdminPage from "./pages/AdminPage";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<PublicPage />} />
-                <Route path="/admin" element={<AdminPage />} />
-                <Route path="*" element={<PublicPage />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<PublicPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="*" element={<PublicPage />} />
+        </Routes>
     );
 }
